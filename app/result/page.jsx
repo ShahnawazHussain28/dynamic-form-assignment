8dc8f0754f0fb33931a6f8cf74690669a3afbcd2
Shahnawaz Hussain <shahnawazhussain2802@gmail.com>
2024-06-23 14:03:20 +0530

    Minor changes

--- app/result/page.jsx
+++ app/result/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 
 export default function Result() {
   const params = useSearchParams();
@@ -17,16 +17,18 @@ export default function Result() {
   }, [params]);
 
   return (
-    <div className="flex flex-col items-center pt-10 mt-24 w-full">
-      <h1 className="text-3xl">Form Data</h1>
-      <div className="p-10 mt-10 text-lg bg-gray-200 border-2 dark:bg-gray-800">
-        {Object.keys(formData).map((key, index) => (
-          <div key={index}>
-            <span className="mr-3">{key} : </span>
-            <span className="">{formData[key]}</span>
-          </div>
-        ))}
+    <Suspense>
+      <div className="flex flex-col items-center pt-10 mt-24 w-full">
+        <h1 className="text-3xl">Form Data</h1>
+        <div className="p-10 mt-10 text-lg bg-gray-200 border-2 dark:bg-gray-800">
+          {Object.keys(formData).map((key, index) => (
+            <div key={index}>
+              <span className="mr-3">{key} : </span>
+              <span className="">{formData[key]}</span>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
